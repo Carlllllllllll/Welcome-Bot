0c0005460d2cf0ddf8c010901c09aa7207fb6889
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-04-19 15:40:48 +0200

    Update setup.js

--- commands/setup.js
+++ commands/setup.js
@@ -135,17 +135,7 @@ module.exports = {
             const { title, description, image, footer, color, footerURL, thumbnail, authorName, authorURL, authorIcon } = processedMessageData;
             
             const welcomeMessage = `Hello ${member}!`;
-            const welcomeMessageDm = `💝 This message has been sent from **${member.guild.name}!**`;
-            
-            const embed = new EmbedBuilder()
-                .setTitle(title)
-                .setDescription(description)
-                .setImage(image)
-                .setColor(color)
-                .setTimestamp()
-                .setFooter({ text: footer, iconURL: footerURL }) 
-                .setThumbnail(thumbnail) 
-                .setAuthor({ name: authorName, iconURL: authorIcon, url: authorURL });
+
             
     
  
